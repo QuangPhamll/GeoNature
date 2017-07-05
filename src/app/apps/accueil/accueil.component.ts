1ffbb90c55be478aacc072c399b7b329879906d5
QuangPhamll <quang.pham.fr@gmail.com>
2017-07-05 11:13:56 +0200

    Add file config global in conf/app.config.ts using as a provider in each component

--- src/app/apps/accueil/accueil.component.ts
+++ src/app/apps/accueil/accueil.component.ts
@@ -1,13 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import {APP_CONFIG , CONFIG, AppConfig} from 'conf/app.config';
 
 @Component({
   selector: 'app-accueil',
   templateUrl: './accueil.component.html',
-  styleUrls: ['./accueil.component.scss']
+  styleUrls: ['./accueil.component.scss'],
+  providers: [{ provide: APP_CONFIG, useValue: CONFIG }]
 })
 export class AccueilComponent implements OnInit {
+  private appName: string;
+  private welcomeMessage: string;
 
-  constructor() { }
+  constructor(@Inject(APP_CONFIG) private config: AppConfig) {
+    this.appName =  config.appName;
+    this.welcomeMessage = config.welcomeMessage;
+    console.log(this.welcomeMessage);
+  }
 
   ngOnInit() {
   }
